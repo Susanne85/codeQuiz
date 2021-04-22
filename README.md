27fb85f251ff230fb0e83511fabb93cd0bced5e8
Susanne Bilney <xkqk2010@gmail.com>
2021-04-22 08:29:18 +0000

    Updated Readme file

--- README.md
+++ README.md
@@ -14,6 +14,28 @@ At this stage of development, user details in local storage are overwritten with
 
 On the final page users can either elect to clear their user score by clicking on the 'Clear' button or clicking on the 'Go Back' button which will restart the quiz.
 
-In the images folder for this application shows the various screen displays.
+Link
+
+The link to the deployed application is : https://susanne85.github.io/codeQuiz/
+
+The following screen shots show the appearance of the application.
+
+![Initial page display](./images/01-start-quiz.png)
+
+![Correct answer display](./images/02-correct-answer.png)
+
+![Final display](./images/03-final.png)
+
+![High Scores display](./images/04-high-scores.png)
+
+![View High Scores display](./images/05-view-high-scores.png)
+
+![View Clear Scores display](./images/06-clear.png)
+
+![View Go Back display](./images/07-go-back.png)
+
+Link to deployed application
+
+https://susanne85.github.io/codeQuiz/
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-                       
+
 let quizListQuestions = $('#quiz-list');
-let userScores = $('#user-scores');                   ;
+let userScores = $('#user-scores');;
 let timeResult = $('#time-result');
 
 let startQuiz = '.btn-start';
@@ -13,7 +13,7 @@ let viewScoresBtn = '.btn-viewScores';
 
 let getStudentScores;
 
-let studentTotal=0;
+let studentTotal = 0;
 let setIndex = 0;
 let timeLeft = 75;
 let timePenalty = 0;
@@ -22,9 +22,9 @@ let waitTime;
 let stopTimer = false;
 
 let questionSet = {
-    question: ['Commonly used DataTypes DO NOT include', 'The condition in an IF/Else statement is enclosed within _____', 'Arrays in Javascript can be used to store ____', 'A very useful tool used during development and debugging for printing content to the debugger is ____', 'String values must be enclosed within ____ when being assigned to variables'],
-    answers: ['strings-boolean-alerts-numbers', 'quotes-curly brackets-square brackets-parenthesis', 'commas-other arrays-booleans-all of the above','javascript-terminal/bash-for loops-console log','commas-curly brackets-quotes-parenthesis'],
-    correctAnswer:['alerts', 'parenthesis', 'all of the above', 'console log','curly brackets']
+  question: ['Commonly used DataTypes DO NOT include', 'The condition in an IF/Else statement is enclosed within _____', 'Arrays in Javascript can be used to store ____', 'A very useful tool used during development and debugging for printing content to the debugger is ____', 'String values must be enclosed within ____ when being assigned to variables'],
+  answers: ['strings-boolean-alerts-numbers', 'quotes-curly brackets-square brackets-parenthesis', 'commas-other arrays-booleans-all of the above', 'javascript-terminal/bash-for loops-console log', 'commas-curly brackets-quotes-parenthesis'],
+  correctAnswer: ['alerts', 'parenthesis', 'all of the above', 'console log', 'curly brackets']
 };
 
 let studentScores = {
@@ -37,14 +37,14 @@ function goBack(event) {
   event.stopPropagation()
   $('#user-scores').empty();
   $('#time-result').empty();
-  
+
   setIndex = 0;
   stopTimer = false;
-  studentTotal=0;
+  studentTotal = 0;
   timeLeft = 75;
   timePenalty = 0;
 
-  clearQuestions ()
+  clearQuestions()
   $("#quiz-list").trigger("reset");
   initialiseQuestions();
 }
@@ -54,24 +54,24 @@ function setTime() {
   let timerResult, timeInterval;
 
   timeInterval = setInterval(function () {
-  timeLeft = timeLeft + timePenalty;
+    timeLeft = timeLeft + timePenalty;
 
-  if (timeLeft < 0) {
-    timeLeft = 0;
-  }
+    if (timeLeft < 0) {
+      timeLeft = 0;
+    }
 
-  timerResult = document.querySelector('#time-result');
-  timerResult.lastElementChild.textContent = timeLeft;
-      
-  timePenalty = 0;
-     
-  if (timeLeft <= 0 || stopTimer) {
-    clearTimeout(waitTime);
-    clearInterval(timeInterval);
-    stopTimer = true;
-    finalScores ();
-  }
-  timeLeft--;
+    timerResult = document.querySelector('#time-result');
+    timerResult.lastElementChild.textContent = timeLeft;
+
+    timePenalty = 0;
+
+    if (timeLeft <= 0 || stopTimer) {
+      clearTimeout(waitTime);
+      clearInterval(timeInterval);
+      stopTimer = true;
+      finalScores();
+    }
+    timeLeft--;
   }, 1000);
 }
 
@@ -88,14 +88,14 @@ function finalScores() {
   quizListQuestions.append(pItem, pItem1, labelItem, inputItem, buttonItem);
 }
 
-function viewScores(event){
+function viewScores(event) {
   console.log('view Scores');
   let score;
 
   event.stopPropagation()
 
-  score  = document.querySelector('.view-scores');
-  console.log ('view-Scores' + score);
+  score = document.querySelector('.view-scores');
+  console.log('view-Scores' + score);
   if (score === null) {
     getHighScores();
     if (getStudentScores !== null) {
@@ -104,17 +104,17 @@ function viewScores(event){
       userScores.append(ulItem, liItem);
     }
   } else {
-   $('.view-scores').remove();    
- }
+    $('.view-scores').remove();
+  }
 }
 
 function clearButton(event) {
   console.log('clearing Button');
   let clearScore;
 
-  clearScore  = document.querySelector('.show-scores');
+  clearScore = document.querySelector('.show-scores');
   if (clearScore !== null) {
-    $('.show-scores').remove(); 
+    $('.show-scores').remove();
     localStorage.removeItem("studentScores");
   }
 }
@@ -128,22 +128,22 @@ function getHighScores(event) {
   }
 }
 
-function saveUserScores(event){
-  console.log('saveUserDetails');
+function saveUserScores(event) {
+  console.log('saveUserDetails', event);
   event.stopPropagation();
 
-  studentScores.initials =document.querySelector('.initials').value;
-  studentScores.score  = studentTotal;
+  studentScores.initials = document.querySelector('.initials').value;
+  studentScores.score = studentTotal;
 
-  studentScores = localStorage.setItem("studentScores", JSON.stringify(studentScores));
-
+  localStorage.setItem("studentScores", JSON.stringify(studentScores));
+   
   clearQuestions();
-  
+
   getHighScores();
 
   ulItem = $('<ul class="show-scores" style="margin-left:55px">').text('High Scores');
   liItem = $('<li class="show-scores" style="margin-left:65px">').text(getStudentScores.initials + ' ' + getStudentScores.score);
-  
+
   buttonItem1 = $('<button class="btn-clean" style="margin:15px">Clear</button>');
   buttonItem2 = $('<button class="btn-goBack" style="margin:40px;">Go Back</button>');
 
@@ -151,7 +151,7 @@ function saveUserScores(event){
 
 }
 
-function clearQuestions () {
+function clearQuestions() {
   $('#quiz-list').empty();
 }
 
@@ -159,16 +159,16 @@ function checkAnswers(event) {
   console.log('checkAnswers ' + timeLeft);
   event.stopPropagation();
   let answerText, questionChk;
-  
-  questionChk     = event.target.innerText;
+
+  questionChk = event.target.innerText;
   answerTextArray = questionSet.correctAnswer[setIndex].split("-");
 
   answerText = "Wrong!";
   timePenalty = -10;
 
-  for (let i=0; i < answerTextArray.length; i++) {
+  for (let i = 0; i < answerTextArray.length; i++) {
     if (answerTextArray[i] === questionChk) {
-      studentTotal = studentTotal +1;
+      studentTotal = studentTotal + 1;
       answerText = "Correct!";
       timePenalty = 0;
     }
@@ -176,69 +176,85 @@ function checkAnswers(event) {
 
   pItem = $('<p>').text(answerText);
   quizListQuestions.append(pItem);
-  
+
   waitTime = setTimeout(askQuestions, 500, event);
 }
 
 function askQuestions(event) {
-  console.log('askQuestions'  + 'stopTimer ' + stopTimer + 'index' + setIndex);
-  
+  console.log('askQuestions' + 'stopTimer ' + stopTimer + 'index' + setIndex);
+
   if (event != null) {
-  
+
     if (event.currentTarget.className === 'btn-start') {
       event.stopPropagation();
       event.preventDefault();
       clearQuestions();
       setTime();
     }
-    else{
+    else {
       setIndex = setIndex + 1;
       clearQuestions();
     }
   }
-
+  
   if (setIndex < questionSet.question.length) {
     let newQuestionItem = $('<ul>');
     let newAnswerText;
 
     newQuestionItem.text(questionSet.question[setIndex]);
     newQuestionItem.appendTo(quizListQuestions);
-  
+
     answerTextArray = questionSet.answers[setIndex].split("-");
-    for (let i=0; i < answerTextArray.length; i++) {
+    for (let i = 0; i < answerTextArray.length; i++) {
       newAnswerText = $('<li button class="btn-select align-left p-2 bg-light text-dark" style="list-style:decimal; margin:15px">');
       newAnswerText.text(answerTextArray[i]);
       newAnswerText.appendTo(quizListQuestions);
     }
   }
   else {
-    stopTimer =true;
+    stopTimer = true;
   }
-}   
+}
 
 function initialiseQuestions(event) {
   console.log('initialise');
- 
+
   buttonItem = $('<button class="btn-viewScores" style="background:purple; margin-left:5px; color:white; font-size:1.5rem">View Scores</button>');
   userScores.append(buttonItem);
 
   h4Item = $('<h4>').text('Time:');
   pItem = $('<p>');
   timeResult.append(h4Item, pItem);
-  
+
   h1Item = $('<h1>').text('Coding Quiz Challenge');
   hrItem = $('<hr>');
   h3Item = $('<h3>').text('Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your core time by ten seconds.');
   buttonItem = $('<button class="btn-start" style="background:purple; margin-left:15px; color:white; font-size:1.5rem">Start Quiz</button>');
-  
+
   quizListQuestions.append(h1Item, hrItem, h3Item, buttonItem);
 }
 
-initialiseQuestions();        
-                
+// InitialQuestions sets up the first page of the Quiz and allows the user to determine when to start the quiz by clicking
+// on the Start Buttton
+
+initialiseQuestions();
+
+// When the user clicks on the Start Button then the quiz commences and a question and the possible answers to that question are
+// displayed
+
 quizListQuestions.on("click", startQuiz, askQuestions);
+
+// When the user has selected a particular answer then it is checked as to whether it is correct.
 quizListQuestions.on("click", answerBtn, checkAnswers);
-quizListQuestions.on("click", submitBtn, saveUserScores);      
+
+// After the end of the quiz the user will have the option to save their score to local storage.
+quizListQuestions.on("click", submitBtn, saveUserScores);
+
+// The user has the option of clearing their score from local storage.
 quizListQuestions.on("click", clearBtn, clearButton);
+
+// The user has the option of restarting the quiz.
 quizListQuestions.on("click", goBackBtn, goBack);
+
+//At the top of each page the user has an option of viewing the scores of students that are stored in local storage.
 userScores.on('click', viewScoresBtn, viewScores);
